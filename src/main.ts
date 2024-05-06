5c68d11d810e1d50f5be1a5f57792691de4b41d4
Henri Kim <hkimpr40@gmail.com>
2024-05-06 00:15:58 -0300

    add blocks.json, mine nonce of blocks file, update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,53 @@
 import CryptoJS from 'crypto-js';
+import fs from "fs";
+import path from 'path';
+const inBlocks = require("../blocks.json");
+
+let resultBlocks = inBlocks;
 
 function hashStr(string: string): string {
     return CryptoJS.SHA256(string).toString(CryptoJS.enc.Hex);
 }
 
-let blockNumber = 0
-let blockData = "teste";
-let nonce = 0;
-let resultHash = "";
+console.log(inBlocks.length+" blocks found. mining...\n\n");
 
-for(let i=0;true;i++) {
-    resultHash = hashStr(blockNumber.toString()+i.toString()+blockData);
-    if(resultHash.substring(0, 4) == "0000") {
-        nonce = i;
-        break;
+for(let block=0;block < inBlocks.length;block++) {
+    if(inBlocks[block].verified == true) {
+        console.log(`block ${block} is verified.\n\n`);
+        continue;
     }
+    let blockNumber = inBlocks[block].number;
+    let nonce = 0
+    let blockData = inBlocks[block].data;
+    let hashWord = "";
+    let resultHash = "";
+
+    for(let i=0;true;i++) {
+        resultHash = hashStr(blockNumber.toString()+i.toString()+blockData);
+        if(resultHash.substring(0, 4) == "0000") {
+            nonce = i;
+            break;
+        }
+    }
+    hashWord = `\x1b[1;35m${blockNumber.toString()}\x1b[0m\x1b[1;36m${nonce.toString()}\x1b[0m\x1b[1;37m${blockData}\x1b[0m`;
+    console.log(
+        `blockNumber: \x1b[1;35m${blockNumber}\x1b[0m\n`+
+        `nonce: \x1b[1;36m${nonce}\x1b[0m\n`+
+        `blockData: \x1b[1;37m\n${blockData}\x1b[0m\n\n`+
+        `hashWord: "${hashWord}"\n`+
+        `hash: \x1b[1;32m${resultHash}\x1b[0m\n\n`
+    );
+    resultBlocks[block].verified = true;
+    resultBlocks[block].nonce = nonce;
+    resultBlocks[block].hash = resultHash;
 }
 
-console.log(
-    `blockNumber: \x1b[1;36m${blockNumber}\x1b[0m\n`+
-    `nonce: \x1b[1;35m${nonce}\x1b[0m\n`+
-    `blockData: \x1b[1;37m\n${blockData}\x1b[0m\n\n`+
-    `hash: \x1b[1;32m${resultHash}\x1b[0m`
-);
+
+try {
+    fs.writeFileSync(path.resolve(__dirname, '../blocksMined.json'), JSON.stringify(resultBlocks, null, 2));
+    console.log("'../blocksMined.json' writed.");
+} catch (error) {
+    console.error("Error writing '../blocksMined.json':", error);
+}
+
+console.log("all blocks verified.");
